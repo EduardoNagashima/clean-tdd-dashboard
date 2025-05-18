eebe3cb7b3c24101ac12e2121eac01f2542312ce
Eduardo Yudji Nagashima Miyata <45578435+EduardoNagashima@users.noreply.github.com>
2025-05-18 12:27:30 +0000

    change jest config and assemble login router file

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   coverageProvider: 'v8',
   testEnvironment: 'node'
 }
---
+++ src/presentation/helpers/http-response.js
@@ -0,0 +1,16 @@
+import { MissingParamError } from './missing-param-error.js'
+
+export class HttpResponse {
+  static badRequest (paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName)
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500
+    }
+  }
+}
